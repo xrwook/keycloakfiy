693f5c4de35aefb349ddf8b7a8900c009e947cea
허욱 <wook@xr-friends.com>
2026-06-22 15:02:58 +0900

    chore: add pnpm workspace configuration to allow esbuild builds

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { KcPage } from "./kc.gen";
 
-// The following block can be uncommented to test a specific page with `yarn dev`
+// The following block can be uncommented to test a specific page with `pnpm dev`
 // Don't forget to comment back or your bundle size will increase
 
 import { getKcContextMock } from "./login/KcPageStory";
