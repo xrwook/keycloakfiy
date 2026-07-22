d2e50fe60bb39c91134cc27ad783706124a5f60c
허욱 <wook@xr-friends.com>
2026-07-22 12:29:30 +0900

    feat: 로그인 페이지 구성 요소 추가 및 스토리북 설정
    - TOTP 로그인 구성 페이지(LoginConfigTotp) 및 관련 스토리(LoginConfigTotp.stories.tsx) 추가
    - OTP 로그인 페이지(LoginOtp) 및 관련 스토리(LoginOtp.stories.tsx) 추가
    - 비밀번호 재설정 페이지(LoginResetPassword) 및 관련 스토리(LoginResetPassword.stories.tsx) 추가
    - 비밀번호 업데이트 페이지(LoginUpdatePassword) 및 관련 스토리(LoginUpdatePassword.stories.tsx) 추가
    - 이메일 확인 페이지(LoginVerifyEmail) 및 관련 스토리(LoginVerifyEmail.stories.tsx) 추가
    - 각 페이지에 대한 다양한 상태(오류, 성공 메시지 등)를 처리하는 스토리 추가

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -9,17 +9,80 @@ const UserProfileFormFields = lazy(
 );
 
 const Register = lazy(() => import("./pages/Register"));
+const Error = lazy(() => import("./pages/Error"));
+const LoginConfigTotp = lazy(() => import("./pages/LoginConfigTotp"));
+const LoginOtp = lazy(() => import("./pages/LoginOtp"));
+const LoginResetPassword = lazy(() => import("./pages/LoginResetPassword"));
+const LoginUpdatePassword = lazy(() => import("./pages/LoginUpdatePassword"));
+const LoginVerifyEmail = lazy(() => import("./pages/LoginVerifyEmail"));
+
 const doMakeUserConfirmPassword = true;
 
 export default function KcPage(props: { kcContext: KcContext }) {
   const { kcContext } = props;
-
+  const Login = lazy(() => import("./pages/Login"));
   const { i18n } = useI18n({ kcContext });
 
   return (
     <Suspense>
       {(() => {
         switch (kcContext.pageId) {
+          case "login-verify-email.ftl":
+            return (
+              <LoginVerifyEmail
+                {...{ kcContext, i18n, classes }}
+                Template={Template}
+                doUseDefaultCss={true}
+              />
+            );
+          case "login-update-password.ftl":
+            return (
+              <LoginUpdatePassword
+                {...{ kcContext, i18n, classes }}
+                Template={Template}
+                doUseDefaultCss={true}
+              />
+            );
+          case "login-reset-password.ftl":
+            return (
+              <LoginResetPassword
+                {...{ kcContext, i18n, classes }}
+                Template={Template}
+                doUseDefaultCss={true}
+              />
+            );
+          case "login-otp.ftl":
+            return (
+              <LoginOtp
+                {...{ kcContext, i18n, classes }}
+                Template={Template}
+                doUseDefaultCss={true}
+              />
+            );
+          case "login-config-totp.ftl":
+            return (
+              <LoginConfigTotp
+                {...{ kcContext, i18n, classes }}
+                Template={Template}
+                doUseDefaultCss={true}
+              />
+            );
+          case "error.ftl":
+            return (
+              <Error
+                {...{ kcContext, i18n, classes }}
+                Template={Template}
+                doUseDefaultCss={true}
+              />
+            );
+          case "login.ftl":
+            return (
+              <Login
+                {...{ kcContext, i18n, classes }}
+                Template={Template}
+                doUseDefaultCss={true}
+              />
+            );
           case "register.ftl":
             return (
               <Register
